829b50162db48db61990efdede874ec5a1767d24
R-R-Freitas <rodrigoaz8@gmail.com>
2022-10-20 20:40:37 -0300

    feat: file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    repeated = False
+    for file in range(len(instance)):
+        if path_file == instance.search(file)["nome_do_arquivo"]:
+            repeated = True
+            break
+    if not repeated:
+        file_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+        instance.enqueue(file_data)
+        print(file_data)
 
 
 def remove(instance):
