8ef2a1bd3b4341ab89ee8a90535e76344f9cdaf6
R-R-Freitas <rodrigoaz8@gmail.com>
2022-10-20 18:47:33 -0300

    feat: classes queue and node

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,51 @@
+# import re
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.head_value = None
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        if self.__length == 0:
+            new_value = Node(value)
+            new_value.next = self.head_value
+            self.head_value = new_value
+            self.__length += 1
+        else:
+            new_value = Node(value)
+            current_value = self.head_value
+            while current_value.next:
+                current_value = current_value.next
+            current_value.next = new_value
+            self.__length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value_to_dequeue = self.head_value
+        if value_to_dequeue:
+            self.head_value = self.head_value.next
+            value_to_dequeue.next = None
+            self.__length -= 1
+        return value_to_dequeue.value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index not in range(self.__length):
+            raise IndexError
+        returned_value = None
+        value_to_return = self.head_value
+        if value_to_return:
+            while index > 0 and value_to_return.next:
+                value_to_return = value_to_return.next
+                index -= 1
+            if value_to_return:
+                returned_value = value_to_return.value
+        return returned_value
