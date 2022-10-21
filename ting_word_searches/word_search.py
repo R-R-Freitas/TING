69fc2d343e608a1add2a146fdd1369592b5c512d
R-R-Freitas <rodrigoaz8@gmail.com>
2022-10-20 22:02:18 -0300

    feat: word_search: search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -11,13 +11,17 @@ def exists_word(word, instance):
                     and results[-1]["arquivo"]
                     == current_file["nome_do_arquivo"]
                 ):
-                    results[-1]["ocorrencias"].append({"linha": line_number})
+                    results[-1]["ocorrencias"].append(
+                        {"linha": line_number}
+                    )
                 else:
                     results.append(
                         {
                             "palavra": word,
                             "arquivo": current_file["nome_do_arquivo"],
-                            "ocorrencias": [{"linha": line_number}],
+                            "ocorrencias": [
+                                {"linha": line_number}
+                            ],
                         }
                     )
             line_number += 1
@@ -26,4 +30,31 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    index = 0
+    while index < len(instance):
+        current_file = instance.search(index)
+        line_number = 1
+        for line in current_file["linhas_do_arquivo"]:
+            if word.lower() in line.lower():
+                if (
+                    len(results) > 0
+                    and results[-1]["arquivo"]
+                    == current_file["nome_do_arquivo"]
+                ):
+                    results[-1]["ocorrencias"].append(
+                        {"linha": line_number, "conteudo": line}
+                    )
+                else:
+                    results.append(
+                        {
+                            "palavra": word,
+                            "arquivo": current_file["nome_do_arquivo"],
+                            "ocorrencias": [
+                                {"linha": line_number, "conteudo": line}
+                            ],
+                        }
+                    )
+            line_number += 1
+        index += 1
+    return results
