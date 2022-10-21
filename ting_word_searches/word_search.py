a93415aaf45149abc122719804ee55b37cc5f01c
R-R-Freitas <rodrigoaz8@gmail.com>
2022-10-20 22:03:11 -0300

    refactor: word_search: exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,31 +1,8 @@
 def exists_word(word, instance):
-    results = []
-    index = 0
-    while index < len(instance):
-        current_file = instance.search(index)
-        line_number = 1
-        for line in current_file["linhas_do_arquivo"]:
-            if word.lower() in line.lower():
-                if (
-                    len(results) > 0
-                    and results[-1]["arquivo"]
-                    == current_file["nome_do_arquivo"]
-                ):
-                    results[-1]["ocorrencias"].append(
-                        {"linha": line_number}
-                    )
-                else:
-                    results.append(
-                        {
-                            "palavra": word,
-                            "arquivo": current_file["nome_do_arquivo"],
-                            "ocorrencias": [
-                                {"linha": line_number}
-                            ],
-                        }
-                    )
-            line_number += 1
-        index += 1
+    results = search_by_word(word, instance)
+    for file in results:
+        for occurrence in file["ocorrencias"]:
+            occurrence.pop("conteudo")
     return results
 
 
